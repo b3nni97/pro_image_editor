5409863308afbb50dc4fb942e4c448fb681e31da
Benjamin Schweizer <bs103@hdm-stuttgart.de>
2026-02-13 04:46:16 +0100

    #

---
+++ croppy/croppy-master/lib/src/image/_image.dart
@@ -0,0 +1,1 @@
+export 'crop_image.dart' if (dart.library.js_interop) 'web_crop_image.dart';
---
+++ croppy/croppy-master/lib/src/widgets/material/theme/material_image_cropper_theme.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+ThemeData generateMaterialImageCropperTheme(BuildContext context) {
+  final outerTheme = Theme.of(context);
+
+  return ThemeData.localize(
+    ThemeData.from(
+      colorScheme: ColorScheme.fromSeed(
+        seedColor: outerTheme.colorScheme.primary,
+        brightness: Brightness.dark,
+      ),
+      useMaterial3: outerTheme.useMaterial3,
+    ),
+    outerTheme.textTheme,
+  ).copyWith(scaffoldBackgroundColor: Colors.black);
+}
